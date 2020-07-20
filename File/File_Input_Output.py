06a9bd0d29370e91d1f07c60a7b3647b093fd088
Alex Costan <acostan.con@orangetheory.com>
2020-07-20 10:16:56 -0400

    file IO errors

--- File/File_Input_Output.py
+++ File/File_Input_Output.py
@@ -1,25 +1,25 @@
-# my_file = open('test.txt')
-#
-# print(my_file)  # <_io.TextIOWrapper name='test.txt' mode='r' encoding='cp1252'>
-#
-# #  Reads entire file
-# print(my_file.read())  # hi my name is alex costan
-# my_file.seek(0)  # starts cursor back to the first line of the .txt file
-# print(my_file.read())  # hi my name is alex costan
-# my_file.seek(0)  # starts cursor back to the first line of the .txt file
-# print(my_file.read())  # hi my name is alex costan
-#
-#
-# #  Reads one line at a time
-# print(my_file.readline(()))  # hi my name is alex costan
-# print(my_file.readline(()))  # :)
-# print(my_file.readline(()))  # how are you?
-#
-# #  Reads all the lines
-# print(my_file.readlines())  # ['hi my name is alex costan\n', ':)\n', 'how are you?']
-#
-# #  Good practice when finished using file
-# my_file.close()
+my_file = open('test.txt')
+
+print(my_file)  # <_io.TextIOWrapper name='test.txt' mode='r' encoding='cp1252'>
+
+#  Reads entire file
+print(my_file.read())  # hi my name is alex costan
+my_file.seek(0)  # starts cursor back to the first line of the .txt file
+print(my_file.read())  # hi my name is alex costan
+my_file.seek(0)  # starts cursor back to the first line of the .txt file
+print(my_file.read())  # hi my name is alex costan
+
+
+#  Reads one line at a time
+print(my_file.readline(()))  # hi my name is alex costan
+print(my_file.readline(()))  # :)
+print(my_file.readline(()))  # how are you?
+
+#  Reads all the lines
+print(my_file.readlines())  # ['hi my name is alex costan\n', ':)\n', 'how are you?']
+
+#  Good practice when finished using file
+my_file.close()
 
 
 # Proper way to work with Python files
@@ -47,3 +47,16 @@ with open('test.txt', mode='r+') as my_file:
 with open('test.txt', mode='a') as my_file:
     text = my_file.write(':)')  # appends ':)' to end of file in 'a' mode
     print(text)  # 2
+
+
+# File IO Errors
+
+try:
+    with open('sad.txt', mode='r') as my_file:
+        print(my_file.read())
+except FileNotFoundError as err:
+    print('file does not exist')
+    raise err
+except IOError as err:
+    print('IO error')
+    raise err
