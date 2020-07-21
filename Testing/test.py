6598224fa8637e08251344916a93ad7a545959fe
Alex Costan <acostan.con@orangetheory.com>
2020-07-21 12:11:52 -0400

    writing unit tests

--- Testing/test.py
+++ Testing/test.py
@@ -3,22 +3,46 @@ from Testing import main
 
 
 class TestMain(unittest.TestCase):
-    # def test_do_stuff(self):
-    #     test_param = 10
-    #     result = main.do_stuff(test_param)
-    #     self.assertEqual(result, 15)  # Ran 1 test in 0.002s, OK
-    #
-    # def test_do_stuff2(self):
-    #     test_param = 10
-    #     result = main.do_stuff(test_param)
-    #     self.assertEqual(result, 10)  # AssertionError: 15 != 10
+    def test_do_stuff(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 15)  # Ran test in 0.002s, OK
 
-    # def test_do_stuff3(self):
-    #     test_param = 'asdf'
-    #     result = main.do_stuff(test_param)
-    #     self.assertEqual(result, ValueError)  # AssertionError: ValueError("invalid literal for int() with base 10: 'asdf'") != <class 'ValueError'>
+    def test_do_stuff2(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 10)  # AssertionError: 15 != 10
+
+    def test_do_stuff3(self):
+        test_param = 'asdf'
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, ValueError)  # AssertionError: ValueError("invalid literal for int() with base 10: 'asdf'") != <class 'ValueError'>
 
     def test_do_stuff4(self):
         test_param = 'asdf'
         result = main.do_stuff(test_param)
-        self.assertTrue(isinstance(result, ValueError))  # Ran 1 test in 0.001s, OK
+        self.assertIsInstance(result, ValueError)  # Ran test in 0.001s, OK
+
+    def test_do_stuff5(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, ValueError)  # TypeError: int() argument must be a string, a bytes-like object or a number, not 'NoneType'
+
+    def test_do_stuff6(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter number')  # Ran test in 0.001s, OK
+
+    def test_do_stuff7(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter number')  # Ran test in 0.001s, OK
+
+    def test_do_stuff8(self):
+        test_param = 0
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'please enter number')  # Ran test in 0.001s, OK
+
+
+if __name__ == '__main__':
+    unittest.main()
